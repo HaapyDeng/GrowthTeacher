9fa22689f2e217e5281d5da3ed4877312b9a6670
HaapyDeng <jzhou_deng@163.com>
2018-09-29 17:59:30 +0800

    UI

--- app/src/main/java/com/mpl/GrowthTeacher/Activity/HelperFragment.java
+++ app/src/main/java/com/mpl/GrowthTeacher/Activity/HelperFragment.java
@@ -1,18 +1,23 @@
 package com.mpl.GrowthTeacher.Activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mpl.GrowthTeacher.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HelperFragment extends Fragment {
+public class HelperFragment extends Fragment implements View.OnClickListener {
+    private ImageView ib_message;
+    private TextView tv_zhonghe, tv_studentchengjiu, tv_tasktongji;
 
     public static HelperFragment newInstance(String name) {
         HelperFragment fragment = new HelperFragment();
@@ -31,8 +36,32 @@ public class HelperFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_helper, container, false);
+        View root = inflater.inflate(R.layout.fragment_helper, container, false);
+        ib_message = root.findViewById(R.id.ib_message);
+        ib_message.setOnClickListener(this);
+        tv_zhonghe = root.findViewById(R.id.tv_zhonghe);
+        tv_zhonghe.setOnClickListener(this);
+        tv_studentchengjiu = root.findViewById(R.id.tv_studentchengjiu);
+        tv_studentchengjiu.setOnClickListener(this);
+        tv_tasktongji = root.findViewById(R.id.tv_tasktongji);
+        tv_tasktongji.setOnClickListener(this);
+        return root;
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_message:
+                Intent intent = new Intent(getActivity(), MessageActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tv_zhonghe:
+                break;
+            case R.id.tv_studentchengjiu:
+                break;
+            case R.id.tv_tasktongji:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/mpl/GrowthTeacher/Activity/StudentGroupFragment.java
+++ app/src/main/java/com/mpl/GrowthTeacher/Activity/StudentGroupFragment.java
@@ -1,18 +1,23 @@
 package com.mpl.GrowthTeacher.Activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.mpl.GrowthTeacher.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StudentGroupFragment extends Fragment {
+public class StudentGroupFragment extends Fragment implements View.OnClickListener {
+
+    private ImageView ib_message;
 
     public static StudentGroupFragment newInstance(String name) {
         StudentGroupFragment fragment = new StudentGroupFragment();
@@ -31,7 +36,19 @@ public class StudentGroupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_group, container, false);
+        View root = inflater.inflate(R.layout.fragment_student_group, container, false);
+        ib_message = root.findViewById(R.id.ib_message);
+        ib_message.setOnClickListener(this);
+        return root;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_message:
+                Intent intent = new Intent(getActivity(), MessageActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
